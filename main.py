ad221a532e9927e0ea8c0a4b0f686d71197d5425
adaliagutierrez <adaliagutierrez31@gmail.com>
2025-05-25 20:28:51 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+def gestionar_clientes():
+    while True:
+        print(" GESTIONAR CLIENTES ")
+        print("1. Ver Clientes")
+        print("2. Agregar Cliente")
+        print("3. Modificar Cliente")
+        print("4. Eliminar Cliente")
+        print("5. Volver al Menú Principal")
+        opcion = input("Seleccione una opción: ")
+
+        if opción == "1":
+
+            for cliente in clientes:
+                print (cliente)
+           
+        else:
+             if opcion == "2":
+                 razon = in
